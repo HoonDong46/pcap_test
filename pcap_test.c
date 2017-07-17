2b306bf7a35b95e521c5891c4e05087b8e1f013b
Kim DongHoon <btg166@naver.com>
2017-07-18 02:12:09 +0900

    add pcap_test

---
+++ pcap_test.c
@@ -0,0 +1,15 @@
+#include <pcap.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+	pcap_t *handle;			/* Session handle */
+	char *dev;			/* The device to sniff on */
+	char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
+	struct bpf_program fp;		/* The compiled filter */
+	char filter_exp[] = "port 80";	/* The filter expression */
+	bpf_u_int32 mask;		/* Our netmask */
+	bpf_u_int32 net;		/* Our IP */
+	struct pcap_pkthdr header;	/* The header that pcap gives us */
+	const u_char *packet;		/* The actual packet */
+}
